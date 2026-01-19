b8ec1603e2eaa7258c56c4c2753c2b1f4fc7993c
Adi2212 <aditya1357924680@gmail.com>
2026-01-19 18:33:20 +0530

    project setup & dependency added

---
+++ src/main/java/com/agridev/AgrisellBackendApplication.java
@@ -0,0 +1,13 @@
+package com.agridev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrisellBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgrisellBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agridev/AgrisellBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agridev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrisellBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
